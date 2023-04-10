1c4e4f247136d00454fa574d5b5c1967e7cc4a07
Mariama <marememboup93@gmail.com>
2023-04-10 22:19:59 +0000

    add_files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Button } from 'react-bootstrap'
 import './App.css';
 
 function App() {
+    return (
+        <div className="form-container d-flex align-items-center justify-content-center ">
+            <Form className="signup-form">
+            <img className="md-4 bootstrap-logo" src="https://cdn.worldvectorlogo.com/logos/bootstrap-4.svg"></img>
+                <h6 className="sign-in">Please sign in</h6>
+                <Form.Group className="form-group">
+                    <Form.Control className="name-input" type="text" placeholder="name" name="name"></Form.Control>
+                    <Form.Control className="email-input" type="text" placeholder="email" name="email" ></Form.Control>
+                    <Button className="submit-button" value="submit" type="submit">submit</Button>
+                </Form.Group>
+            </Form>
+        </div>
+    )
+}
+export default App;
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +39,5 @@ function App() {
     </div>
   );
 }
-
-export default App;
+//ca permet de pouvoir 
+export default App;*/
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//  ca permet de creer une route a travers id root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//cest ce que contient(ou le rendu) de notre root recemment creer
 root.render(
   <React.StrictMode>
     <App />
